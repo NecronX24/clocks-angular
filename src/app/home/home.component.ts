e031c336846dcf1a9a2078ebbe6bc547b1b31985
Sebangel <angeljavicueche@gmail.com>
2025-06-28 23:39:44 -0400

    feat: se añade vista de Slideshow

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,41 @@ import { RainbowCardClockComponent } from "../rainbow-card/rainbow-card-clock.co
 import { SevenSegmentsClockComponent } from "../seven-segments-clock/seven-segments-clock.component";
 import { StarClockComponent } from "../star-clock/star-clock.component";
 import { TextClockComponent } from "../text-clock/text-clock.component";
+import { NgIf, NgFor } from '@angular/common'; // Import NgIf and NgFor
 
 @Component({
   selector: 'app-home',
-  imports: [AnalogClockComponent, BinaryClockComponent, DigitalClockComponent, DigitalHourglassClockComponent, FlowerClockComponent, LoadingBarClockComponent, RainbowCardClockComponent, SevenSegmentsClockComponent, StarClockComponent, TextClockComponent],
+  standalone: true, // If your component is not standalone, remove this line.
+  imports: [
+    AnalogClockComponent,
+    BinaryClockComponent,
+    DigitalClockComponent,
+    DigitalHourglassClockComponent,
+    FlowerClockComponent,
+    LoadingBarClockComponent,
+    RainbowCardClockComponent,
+    SevenSegmentsClockComponent,
+    StarClockComponent,
+    TextClockComponent,
+    NgIf, // Add NgIf to imports
+    NgFor // Add NgFor to imports
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  currentSlide: number = 0;
+  totalSlides: number[] = Array(10).fill(0); // Assuming you have 10 clock components
 
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.totalSlides.length) % this.totalSlides.length;
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.totalSlides.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+  }
 }
